1914ea2667de83a3e9ac7b6d19a2fd22c92ea9b5
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-10-04 13:47:05 +0200

    instagram-clone.improves

---
+++ script.js
@@ -0,0 +1,32 @@
+let posts = [
+    {   'author': 'Marc Salzhügel',
+        'location': 'Bolivien',
+        'image': './img/',
+        'description': 'The concept of JSON overwhelms me (emoji)',
+        'comments': [{'Jason Statham': '(emoji)'},{'rndm.guy243': 'I guess the JSON Array is not the only problem (emoji)'}]
+    },
+    {   'author': 'Developer-Akademie',
+        'location': 'München',
+        'image': './img/',
+        'description': 'Werde jetzt Software Developer mit unserer Hilfe!',
+        'comments': [{'LifetimeHarzler': 'Man kann niemals in 9 Monaten das lernen was andere in 4 Jahren lernen SCAM!!'},{'bachelorOfBP': 'Also ich habe Informatik studiert und bin jetzt Bierpongkönig'}]
+    },
+    {   'author': 'Sandro.G',
+        'location': 'Zürich',
+        'image': './img/',
+        'description': 'Amore mio',
+        'comments': [{'sbyl.82': 'So Süess OMG!!'}, {'seckin.ap': 'Die chlii Terroristin wird Erwachse :D'}]
+    },
+    {   'author': 'anii.nova',
+        'location': 'Zürich',
+        'image': './img/',
+        'description': '',
+        'comments': ['','']
+    },
+    {   'author': '',
+        'location': '',
+        'image': './img/',
+        'description': '',
+        'comments': ['','']
+    }
+];
