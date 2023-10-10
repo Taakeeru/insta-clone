79c931766243b56b7f2e01b1192bee18ed89142b
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-10-10 14:38:29 +0200

    insta-clone.final-steps

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-let w = Math.floor((Math.random() * 78947) + 1);
-let x = Math.floor((Math.random() * 1567) + 1);
-let y = Math.floor((Math.random() * 141) + 1);
-let z = Math.floor((Math.random() * 103) + 1);
 let posts = [
     {   'profileimg': './img/profile-post1.jpg',
         'author': 'mark.salzhügel',
         'location': 'Bolivien',
         'image': './img/markimg.PNG',
-        'isLiked': 'false',
-        'likes': `${w}`,
+        'isLiked': true,
+        'likes': `47400`,
         'description': 'The concept of JSON overwhelms me &#128555',
         'hashtags': '#SQL #Datamining',
         'comments': [{'name': 'Jason Statham', 'comment': '&#128526 &#128526'}, {'name': 'rndm.guy243', 'comment': "I suspect the JSON Array isn't the sole issue you're facing. &#129315 &#129315"}, {'name': 'jason_derulo_official', 'comment': '&#127925 Jaaasoon Deruloo &#127925'}]
@@ -17,8 +13,8 @@ let posts = [
         'author': 'Developer-Akademie',
         'location': 'München',
         'image': './img/dev-akademieimg.PNG',
-        'isLiked': 'false',
-        'likes': `${x}`,
+        'isLiked': false,
+        'likes': `1403`,
         'description': 'Werde jetzt Software Developer mit unserer Hilfe! <a href="https://developerakademie.com">https://developerakademie.com</a> ',
         'hashtags': '',
         'comments': [{'name': 'LifetimeHarzler', 'comment': 'Man kann niemals in 9 Monaten das lernen was andere in 4 Jahren lernen 100% SCAM!!'}, {'name': 'bachelorOfBP','comment': 'Also ich habe Informatik studiert und bin jetzt Bierpongkönig und ja...'}, {'name': 'katy.rzs','comment': 'Wieviel kostet die Weiterbildung??'}]
@@ -27,8 +23,8 @@ let posts = [
         'author': 'sndr.g',
         'location': 'Zürich',
         'image': './img/post3.PNG',
-        'isLiked': 'true',
-        'likes': `${y}`,
+        'isLiked': true,
+        'likes': `214`,
         'description': `Amore mio &#x2764;&#xFE0F &#x2764;&#xFE0F &#x2764;&#xFE0F`,
         'hashtags': '',
         'comments': [{'name': 'sbyl.82', 'comment': 'So Süess OMG!!'}, {'name': 'seckin.ap', 'comment': 'Jöö die chlii Terroristin wachst soo schnell &#129392'}] //seckin.ap = posts[i(3)]['comments'][j(2)]['name']
@@ -37,8 +33,8 @@ let posts = [
         'author': 'anii.nova',
         'location': 'Zürich',
         'image': './img/post4.PNG',
-        'isLiked': 'true',
-        'likes': `${z}`,
+        'isLiked': false,
+        'likes': `85`,
         'description': '',
         'hashtags': '#Zürich #Hooka',
         'comments': ''
@@ -55,6 +51,7 @@ function render() {
         const post = posts[i];
         content.innerHTML += cardTemplate(i, post);
 
+        checkLike(i);
         
         let comments = document.getElementById(`comments${i}`);
 
@@ -89,6 +86,18 @@ function handleKeyPress(index, event) {
   if (event.key === "Enter") {
     addComment(index);
   }
+}
+
+
+function toggleInputButton(i) {
+  let input = document.getElementById(`card-input${i}`);
+  let button = document.getElementById(`card-button${i}`);
+
+    if (input.value.trim() !== '') {
+      button.classList.remove('d-none');
+    } else {
+      button.classList.add('d-none');
+    }
 }
 
 
@@ -127,13 +136,53 @@ function handleKeyPress(index, event) {
 // }
 
 
-function changeLike(index) {
-  let heart = document.getElementById(`heart${index}`)
+function checkLike(index) {
+  let isLiked = posts[index]['isLiked'];
+  let heart = document.getElementById(`heart${index}`);
 
-    if (heart.src == "./img/icons/heart.svg") {
-      heart.src += "./img/icons/heart-red.png";
-      render();
-    }
+  if (isLiked === true) {
+    heart.src = "img/icons/heart-red.png";
+    heart.classList.remove('white-outline')
+  } else {
+    heart.src = "img/icons/heart.svg";
+    heart.classList.add('white-outline')
+  }
+}
+
+
+// mit einem klick -> filerwechsel,bildwechsel,booleanwechsel und speichern/laden, likeanzahl -/+ speichern/laden 
+function changeLikeNumber(index) {
+  let isLiked = posts[index]['isLiked'];
+  let currentLikes = parseInt(posts[index]['likes']);
+
+  if (isLiked) {
+    currentLikes--;
+  } else {
+    currentLikes++;
+  }
+
+  posts[index]['likes'] = currentLikes.toString();
+
+  let likes = document.getElementById(`likes${index}`);
+  likes.innerHTML = /*html*/`<span>Gefällt ${currentLikes} Mal</span>`;
+
+  posts[index]['isLiked'] = !isLiked;
+
+  save();
+}
+
+function changeLike(index) {
+
+  changeLikeNumber(index);
+
+  let heart = document.getElementById(`heart${index}`);
+  if (posts[index]['isLiked']) {
+    heart.src = "img/icons/heart-red.png";
+    heart.classList.remove('white-outline');
+  } else {
+    heart.src = "img/icons/heart.svg";
+    heart.classList.add('white-outline');
+  }
 }
 
 
@@ -142,24 +191,13 @@ function save() {
   localStorage.setItem('posts', postsAsText);
 }
 
+
 function load() {
   let postsAsText = localStorage.getItem('posts');
 
   if (postsAsText) {
     posts = JSON.parse(postsAsText); 
   }
-}
-
-
-function toggleInputButton(i) {
-  let input = document.getElementById(`card-input${i}`);
-  let button = document.getElementById(`card-button${i}`);
-
-    if (input.value.trim() !== '') {
-      button.classList.remove('d-none');
-    } else {
-      button.classList.add('d-none');
-    }
 }
 
 
@@ -194,18 +232,19 @@ function cardTemplate(index, post) {
               <div class="card-image">
                 <img src="${post['image']}" alt="Account Bild" />
               </div>
-              <div class="card-icon-row white-outline">
+              <div class="card-icon-row">
                 <div class="icon-margin">
-                  <img id="heart${index}" onclick="changeLike(${index})" src="./img/icons/heart.svg" alt="Herz Icon" /><img
+                  <img id="heart${index}" class="white-outline" onclick="changeLike(${index})" src="" alt="Herz Icon" /><img
+                    class="white-outline"
                     src="./img/icons/comment.svg"
                     alt="Chat Icon"
-                  /><img src="./img/icons/share.svg" alt="Teilen Icon" />
+                  /><img class="white-outline" src="./img/icons/share.svg" alt="Teilen Icon" />
                 </div>
                 <div>
-                  <img src="./img/icons/bookmark.svg" alt="Buchmarke Icon" />
+                  <img class="white-outline" src="./img/icons/bookmark.svg" alt="Buchmarke Icon" />
                 </div>
               </div>
-              <div class="card-likes"><span>Gefällt ${post['likes']} Mal</span></div>
+              <div class="card-likes" id="likes${index}"><span>Gefällt ${post['likes']} Mal</span></div>
               <div class="card-description"><span>${post['description']}</span></div>
               <div class="card-hashtags"><a>${post['hashtags']}</a></div>
               <div class="card-comments" id="comments${index}">
